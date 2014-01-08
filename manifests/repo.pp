d87665a36d2d75ef6b09de60c60c481367376b9b
mattRedbox <matt@redboxresearchdata.com.au>
2014-01-09 08:28:55 +1000

    removed gpg file check.

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,15 +1,10 @@
 class nagios-server::repo {
-  $gpg_file_location = "/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6"
   $rpm_name = "rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm"
 
   package { 'yum-plugin-priorities' :
     ensure => installed,
   }
 
-  file { "$gpg_file_location" :
-    ensure => present,
-  }
-  
   case $operatingsystem {
     'centos' : {
       exec { "wget http://packages.sw.be/rpmforge-release/$rpm_name -O /tmp/$rpm_name":
