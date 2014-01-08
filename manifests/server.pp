2ca0878cd2c502e3609d294007a0539e1f55bef8
mattRedbox <matt@redboxresearchdata.com.au>
2014-01-08 10:16:56 +1000

    Added server manifest.

---
+++ manifests/server.pp
@@ -0,0 +1,28 @@
+class nagios-server::server {
+  package { ["nagios","nagios-plugins","nagios-plugins-nrpe"]:
+    ensure => installed,
+  }
+
+  service { nagios:
+    ensure  => running,
+    enable  => true,
+    require => Exec['make-nag-cfg-readable'],
+  }
+
+  # This is because puppet writes the config files so nagios can't read them
+  exec {'make-nag-cfg-readable':
+    command => "find /etc/nagios -type f -name '*cfg' | xargs chmod +r",
+  }
+
+  file { 'resource-d':
+    path   => '/etc/nagios/resource.d',
+    ensure => directory,
+    owner  => 'nagios',
+  }
+
+  # Collect the nagios_host resources
+  Nagios_host <<||>> {
+    require => File[resource-d],
+    notify  => [Exec[make-nag-cfg-readable],Service[nagios]],
+  }
+}
