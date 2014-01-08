81e9b5f07d774dc9ce762ec064b2db27ca4bad81
mattRedbox <matt@redboxresearchdata.com.au>
2014-01-08 14:19:54 +1000

    added operating system check as Centos only.

---
+++ manifests/repo.pp
@@ -0,0 +1,20 @@
+class nagios-server::repo {
+  $gpg_file_location = "/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6"
+
+  case $operatingsystem {
+    'centos' : {
+      yumrepo { 'rpmforge-release' :
+        baseurl => 'http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm',
+        priority  => 10,
+        timeout   => 60,
+        enabled   => 1,
+        gpgkey    => "file://${gpg_file_location}",
+        gpgcheck  => 1,
+        require   => [
+          Package['yum-priorities'],
+          File[${gpg_file_location}]
+        ],
+      }
+    }
+  }
+}
