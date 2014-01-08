251498f98d214de74e1269d147759fc6c1ef3b17
mattRedbox <matt@redboxresearchdata.com.au>
2014-01-08 10:13:22 +1000

    Moved server into its own module. Added path for exec. Corected syntax error in export module.

--- Modulefile
+++ Modulefile
@@ -1,4 +1,4 @@
-name    'nagios'
+name    'nagios-server'
 version '0.1.0'
 source 'UNKNOWN'
 author 'puppet'
--- manifests/init.pp
+++ manifests/init.pp
@@ -36,30 +36,17 @@
 # Copyright 2014 Your name here, unless otherwise noted.
 #
 class nagios-server {
-  package { ["nagios","nagios-plugins","nagios-plugins-nrpe"]:
-    ensure => installed,
+  Exec { 
+    path => [
+      '/usr/local/bin',
+      '/opt/local/bin',
+      '/usr/bin',
+      '/usr/sbin',
+      '/bin',
+      '/sbin'], 
+    logoutput => true,
   }
+  ->
+  class {'server':}
 
-  service { nagios:
-    ensure  => running,
-    enable  => true,
-    require => Exec['make-nag-cfg-readable'],
-  }
-
-  # This is because puppet writes the config files so nagios can't read them
-  exec {'make-nag-cfg-readable':
-    command => "find /etc/nagios -type f -name '*cfg' | xargs chmod +r",
-  }
-
-  file { 'resource-d':
-    path   => '/etc/nagios/resource.d',
-    ensure => directory,
-    owner  => 'nagios',
-  }
-
-  # Collect the nagios_host resources
-  Nagios_host <<||>> {
-    require => File[resource-d],
-    notify  => [Exec[make-nag-cfg-readable],Service[nagios]],
-  }
 }
