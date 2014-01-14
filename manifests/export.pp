cb758af9a014a49283110dfb7c479d2015c0e0ff
mattRedbox <matt@redboxresearchdata.com.au>
2014-01-14 10:42:50 +1000

    added file to agent config.

--- manifests/export.pp
+++ manifests/export.pp
@@ -6,29 +6,29 @@ class nagios-server::export {
           path => "$config_dir/nagios.cfg",
           line => "cfg_file=$resource_dir/$::fqdn",
       }
-
+      
       @@nagios_host { $::fqdn:
          ensure  => present,
          alias   => $hostname,
          address => $ipaddress,
          use     => 'linux-server',
-         target  => "$resource_dir/$::fqdn",
+         target  => "$resource_dir/$::fqdn/host.cfg",
          require => File_line['add_resource_dir'],
       }
-
+      
       @@nagios_hostextinfo { $::fqdn:
          ensure => present,
          icon_image_alt => $operatingsystem,
          icon_image => "base/$operatingsystem.png",
          statusmap_image => "base/$operatingsystem.gd2",
-         target  => "$resource_dir/$::fqdn",
+         target  => "$resource_dir/$::fqdn/hostextinfo.cfg",
          require => File_line['add_resource_dir'],
       }
 
       @@nagios_service { "$::fqdn-check_ping":
          use => 'local-service',
          host_name => "$::fqdn",
-         target  => "$resource_dir/$::fqdn",
+         target  => "$resource_dir/$::fqdn/check_ping.cfg",
          require => File_line['add_resource_dir'],
       }
 }
