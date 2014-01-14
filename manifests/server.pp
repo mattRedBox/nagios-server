26ac1c8e8584fb8de989e188acd3077305e492e9
mattRedbox <matt@redboxresearchdata.com.au>
2014-01-14 12:29:31 +1000

    corrected cyclic dependency.

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,6 +2,13 @@ class nagios-server::server {
   package { ["nagios","nagios-plugins","nagios-plugins-nrpe"]:
     ensure => installed,
   }
+  ->
+  service { nagios:
+    ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
+  }
 
   file { 'resource.d':
     path   => "$defaults::resource_dir",
@@ -12,14 +19,6 @@ class nagios-server::server {
   file_line { 'resource.d':
     path => "$defaults::config_dir/nagios.cfg",
     line => "cfg_dir=$defaults::resource_dir",
-  }
-  ->
-  service { nagios:
-    ensure     => running,
-    enable     => true,
-    hasstatus  => true,
-    hasrestart => true,
-    require    => [Package['nagios']],
   }
   ->
   # Collect the nagios_host resources
