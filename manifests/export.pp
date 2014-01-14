b64024cf3d6a18317471d7a3da4eb6b0c338755d
mattRedbox <matt@redboxresearchdata.com.au>
2014-01-14 11:31:02 +1000

    removed require as no longer used.

--- manifests/export.pp
+++ manifests/export.pp
@@ -8,7 +8,6 @@ class nagios-server::export {
          address => $ipaddress,
          use     => 'linux-server',
          target  => "$resource_dir/$::fqdn_host.cfg",
-         require => File_line['add_resource_dir'],
       }
       
       @@nagios_hostextinfo { $::fqdn:
@@ -17,13 +16,11 @@ class nagios-server::export {
          icon_image => "base/$operatingsystem.png",
          statusmap_image => "base/$operatingsystem.gd2",
          target  => "$resource_dir/$::fqdn_hostextinfo.cfg",
-         require => File_line['add_resource_dir'],
       }
 
       @@nagios_service { "$::fqdn-check_ping":
          use => 'local-service',
          host_name => "$::fqdn",
          target  => "$resource_dir/$::fqdn_check_ping.cfg",
-         require => File_line['add_resource_dir'],
       }
 }
