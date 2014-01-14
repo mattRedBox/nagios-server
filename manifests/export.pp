2d726c8fbb3d540da526fcee62964f410e4f8f39
mattRedbox <matt@redboxresearchdata.com.au>
2014-01-14 11:28:40 +1000

    updated to use host as filename prefix.

--- manifests/export.pp
+++ manifests/export.pp
@@ -2,17 +2,12 @@ class nagios-server::export {
     
   include nagios-server::defaults
 
-      file_line { 'add_resource_dir':
-          path => "$config_dir/nagios.cfg",
-          line => "cfg_file=$resource_dir/$::fqdn",
-      }
-      
       @@nagios_host { $::fqdn:
          ensure  => present,
          alias   => $hostname,
          address => $ipaddress,
          use     => 'linux-server',
-         target  => "$resource_dir/$::fqdn/host.cfg",
+         target  => "$resource_dir/$::fqdn_host.cfg",
          require => File_line['add_resource_dir'],
       }
       
@@ -21,14 +16,14 @@ class nagios-server::export {
          icon_image_alt => $operatingsystem,
          icon_image => "base/$operatingsystem.png",
          statusmap_image => "base/$operatingsystem.gd2",
-         target  => "$resource_dir/$::fqdn/hostextinfo.cfg",
+         target  => "$resource_dir/$::fqdn_hostextinfo.cfg",
          require => File_line['add_resource_dir'],
       }
 
       @@nagios_service { "$::fqdn-check_ping":
          use => 'local-service',
          host_name => "$::fqdn",
-         target  => "$resource_dir/$::fqdn/check_ping.cfg",
+         target  => "$resource_dir/$::fqdn_check_ping.cfg",
          require => File_line['add_resource_dir'],
       }
 }
