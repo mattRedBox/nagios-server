0998557a66a0e62f7992d06065cea3abdb331836
mattRedbox <matt@redboxresearchdata.com.au>
2014-01-16 09:10:35 +1000

    removed client manifests as in separate repo.

--- manifests/export/default.pp
+++
@@ -1,6 +0,0 @@
-class nagios-server::export::default {
-  include nagios-server::export::file    
-  class { 'nagios-server::export::host': }
-  class { 'nagios-server::export::hostextinfo': }
-  class { 'nagios-server::export::host::ping': }
-}
