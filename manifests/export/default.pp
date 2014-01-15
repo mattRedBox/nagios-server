39f576ce0fce25f53538b94aee01c78c4d5682b5
mattRedbox <matt@redboxresearchdata.com.au>
2014-01-15 13:39:21 +1000

    added file typedef.

--- manifests/export/default.pp
+++ manifests/export/default.pp
@@ -1,5 +1,5 @@
 class nagios-server::export::default {
-    
+  include nagios-server::export::file    
   class { 'nagios-server::export::host': }
   class { 'nagios-server::export::hostextinfo': }
   class { 'nagios-server::export::host::ping': }
